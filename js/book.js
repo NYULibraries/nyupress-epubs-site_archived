0621290e0b96b6fef418619cb83ca75113e8e291
Alberto Ortiz Flores <dismorfo@gmail.com>
2014-05-12 17:57:33 -0400

    book.js

--- js/book.js
+++ js/book.js
@@ -13,7 +13,8 @@ YUI().use(
     
     if (match && match[1]) {
 
-        bookURL = 'http://localhost:8080/?epub=epub_content/' + encodeURIComponent(match[1]) + '&embedded=true'
+        // bookURL = 'http://localhost:8080/?epub=epub_content/' + encodeURIComponent(match[1]) + '&embedded=true'
+        bookURL = 'http://dev-dl-pa.home.nyu.edu/NYUPressOA/?epub=epub_content/' + encodeURIComponent(match[1]) + '&embedded=true'
 
         iframe.set('src', bookURL)
 
